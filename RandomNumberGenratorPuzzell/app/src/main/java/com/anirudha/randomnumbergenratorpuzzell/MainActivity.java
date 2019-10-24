d86f3ba86a6eb4aecaafda83247fdd54134e30b7
anirudhagaikwad <gaikwad.anirudha@gmail.com>
2019-10-24 17:44:46 +0530

    Guess RandomNumber

---
+++ RandomNumberGenratorPuzzell/app/src/main/java/com/anirudha/randomnumbergenratorpuzzell/MainActivity.java
@@ -0,0 +1,55 @@
+package com.anirudha.randomnumbergenratorpuzzell;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+//import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    int random;
+
+    public void randomnumber()
+        {
+        random = (int)(Math.random()*((20-1)+1))+1;
+
+        /*Random rand=new Random();
+        random=rand.nextInt(20)+1;*/
+    }
+
+    public void randomgenerate(View v)
+    {
+        try {
+            EditText number = (EditText) findViewById(R.id.puzzelInputText);
+            int guessVal = Integer.parseInt(number.getText().toString());
+            String message;
+            if (random < guessVal) {
+                message = "Random-Number is Lower";
+            } else if (random > guessVal) {
+                message = "Random-Numebr is Higher";
+            } else {
+                message = "Correct-Guess!";
+                randomnumber();
+            }
+            Toast.makeText(this, "hey!"+message, Toast.LENGTH_SHORT).show();
+
+        }
+        catch(Exception e)
+        {
+            Log.i("Exception","Occured in :"+e);
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        randomnumber();
+    }
+}
