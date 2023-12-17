a67a2e8d9ad91e7cffbf46b57c8e73aa671e51f5
anirudhagaikwad <gaikwad.anirudha@gmail.com>
2023-12-17 22:29:11 +0530

    Note3

---
+++ Projects/Java/PuzzelGame1_Java/app/src/main/java/com/anirudha/puzzelgame1_java/MainActivity.java
@@ -0,0 +1,18 @@
+package com.anirudha.puzzelgame1_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("test","hello");
+
+    }
+}
